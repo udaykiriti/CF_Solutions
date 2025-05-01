c2aef7975c209b17f776b511864408e31252a867
uday <udaykiriti624@gmail.com>
2025-05-01 16:17:53 +0530

    updated 1343A.cpp

--- 1343A.cpp
+++ 1343A.cpp
@@ -1,32 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 #define ll long long
 #define endl '\n'
-#define debug(n) cout<<(n)<<endl;
+#define debug(n) cout << (n) << endl;
 const ll INF = 2e18 + 99;
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
 
-int main(){
+int main()
+{
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
   ll arr[32];
-  for(int i = 0; i < 32; i++){
-    arr[i] = pow(2, i+1) - 1;
-  }
+  FOR(i, 0, 32)
+  arr[i] = (1LL << (i + 1)) - 1;
+
   ll t;
-  cin>>t;
-  while(t--){
+  cin >> t;
+  while (t--)
+  {
     ll n;
-    cin>>n;
-    for(int i = 1; i < 32; i++){
-      if(n % arr[i] == 0){
-        cout<<(n/arr[i])<<endl;
-        break;
-      }
+    cin >> n;
+    FOR(i, 1, 32)
+    if (n % arr[i] == 0)
+    {
+      cout << n / arr[i] << endl;
+      break;
     }
-
   }
-
 }
