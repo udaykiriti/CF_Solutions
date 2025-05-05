5cc029ee4fede350746384f0db628ad7b4c94907
uday <udaykiriti624@gmail.com>
2025-05-05 16:07:53 +0530

    124A.cpp 1373B.cpp  1476A.cpp added

---
+++ 124A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define FastIO()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+
+void solve()
+{
+  int n, a, b, c, r;
+  cin >> n >> a >> b;
+  c = n - a - 1;
+
+  if (c > b)
+  {
+    r = b + 1;
+  }
+  else
+  {
+    r = c + 1;
+  }
+
+  cout << r << endl;
+}
+
+int main()
+{
+  FastIO();
+  solve();
+  return 0;
+}
---
+++ 1373B.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define FastIO()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORk(i, a, b, k) for (int i = (a); i <= (b); i += (k))
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= (k))
+
+void solve()
+{
+  int one = 0, zero = 0;
+  string s;
+  cin >> s;
+  int n = s.length();
+  for (int i = 0; i < n; i++)
+  {
+    if (s[i] == '1')
+      one++;
+    else
+      zero++;
+  }
+  int turn = min(zero, one);
+  if (turn == 0 || turn % 2 == 0)
+  {
+    cout << "NET\n";
+  }
+  else
+  {
+    cout << "DA\n";
+  }
+}
+
+int main()
+{
+  FastIO();
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
