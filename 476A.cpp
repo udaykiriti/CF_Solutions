ea2af618cb921b8ce100b22c56ea91eab620fae2
uday <udaykiriti624@gmail.com>
2025-05-05 14:28:10 +0530

    added

---
+++ 476A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define FastIO()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORk(i, a, b, k) for (int i = (a); i <= (b); i += (k))
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= (k))
+
+void solve()
+{
+  int n, m;
+  cin >> n >> m;
+
+  int min_moves = (n + 1) / 2;
+
+  for (int k = min_moves; k <= n; k++)
+  {
+    if (k % m == 0)
+    {
+      cout << k << endl;
+      return;
+    }
+  }
+
+  cout << -1 << endl;
+}
+
+int main()
+{
+  FastIO();
+  solve();
+  return 0;
+}
---
+++ 742A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define FastIO()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORk(i, a, b, k) for (int i = (a); i <= (b); i += (k))
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= (k))
+
+void solve()
+{
+  int n;
+  cin >> n;
+  if (n == 0)
+  {
+    cout << 1 << endl;
+    return;
+  }
+  int cycle[] = {8, 4, 2, 6};
+  cout << cycle[(n - 1) % 4] << endl;
+}
+
+int main()
+{
+  FastIO();
+  solve();
+  return 0;
+}
