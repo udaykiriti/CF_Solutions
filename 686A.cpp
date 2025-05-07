346e140cb43f375cf58d8b8cb0c9e0d16c6c81ee
uday <udaykiriti624@gmail.com>
2025-05-07 19:42:20 +0530

    added

---
+++ 686A.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+int main()
+{
+  unsigned char op;
+  unsigned int n, d_ch = 0;
+  unsigned long long i_cr, c;
+
+  std::cin >> n >> i_cr;
+
+  for (unsigned int i = 0; i < n; ++i)
+  {
+    std::cin >> op >> c;
+
+    if (op == '+')
+    {
+      i_cr += c;
+    }
+    else
+    {
+      if (c > i_cr)
+      {
+        d_ch++;
+      }
+      else
+      {
+        i_cr -= c;
+      }
+    }
+  }
+
+  std::cout << i_cr << " " << d_ch << std::endl;
+
+  return 0;
+}
