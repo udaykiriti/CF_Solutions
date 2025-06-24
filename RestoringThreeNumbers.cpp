d8c8746a5ca18751d2196fb200f15b2c8e081d42
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 12:36:23 +0530

    restoring three numbers

--- RestoringThreeNumbers.cpp
+++ RestoringThreeNumbers.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
 
 int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
   int arr[4];
-  for(int i = 0; i < 4; i++){
+  FOR(i,0,4){
     cin>>arr[i];
   }
   sort(arr, arr+4);
-  for(int i = 0; i < 3; i++){
+  FOR(i,0,3){
     cout<<(arr[3] - arr[i])<<" ";
   }
-
 }
