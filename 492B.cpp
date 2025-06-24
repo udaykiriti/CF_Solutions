47af133ea50042ee1a67458c7d8585ba466dc509
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 19:54:44 +0530

    added

--- 492B.cpp
+++ 492B.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
+
 
 void solve()
 {
-  int n, l;
-  cin >> n >> l;
+  int n, l; cin >> n >> l;
   vector<int> lanterns(n);
 
   for (int i = 0; i < n; i++)
-  {
     cin >> lanterns[i];
-  }
 
   sort(lanterns.begin(), lanterns.end());
-
   double max_dist = 0;
 
   for (int i = 1; i < n; i++)
-  {
     max_dist = max(max_dist, (double)(lanterns[i] - lanterns[i - 1]) / 2);
-  }
 
   max_dist = max(max_dist, (double)lanterns[0] - 0);
   max_dist = max(max_dist, (double)l - lanterns[n - 1]);
