42b7bb3c1a04495dca431cac4d4961ed2cb9ec96
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 12:24:26 +0530

    Short substrings

--- ShortSubstrings.cpp
+++ ShortSubstrings.cpp
@@ -1,17 +1,16 @@
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
 
 int main(){
-  int t;
-  cin>>t;
+  int t; cin>>t;
   string s;
   while(t--){
     cin>>s;
     cout<<s[0];
-    for(int i = 1; i < s.size();i += 2){
+    for(int i = 1; i < s.size();i += 2)
       cout<<s[i];
-    }
     cout<<endl;
   }
 }
