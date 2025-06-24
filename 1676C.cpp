8a1b3cfc4bee3b9a45697be321548c91f66cc3e0
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 23:20:17 +0530

    added

--- 1676C.cpp
+++ 1676C.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+#define debug(x) cout<<(x)<<endl
 
 int strDiff(string x, string y)
 {
-  int sum = 0;
-  int N = x.size();
+  int sum = 0; int N = x.size();
   for (int i = 0; i < N; i++)
     sum += abs(x[i] - y[i]);
   return sum;
@@ -14,33 +14,24 @@ int strDiff(string x, string y)
 void FastIO()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 }
 
 int main()
 {
   FastIO();
-  int t;
-  cin >> t;
+  int t; cin >> t;
   while (t--)
   {
-    int n, m;
-    cin >> n >> m;
+    int n, m; cin >> n >> m;
     string s[n];
     for (int i = 0; i < n; i++)
-    {
       cin >> s[i];
-    }
     int mini = INT_MAX;
     for (int i = 0; i < n; i++)
-    {
       for (int j = 0; j < n; j++)
-      {
         if (i != j)
           mini = min(mini, strDiff(s[i], s[j]));
-      }
-    }
     cout << mini << endl;
   }
 }
