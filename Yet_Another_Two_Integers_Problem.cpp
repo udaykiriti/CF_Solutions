b8f3bc3e0b2c2760cb748922d985ba342e16d8fd
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 12:17:07 +0530

    panagram

--- Yet_Another_Two_Integers_Problem.cpp
+++ Yet_Another_Two_Integers_Problem.cpp
@@ -1,13 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int a, b;
-    cin>>a;
-    cin>>b;
+    int a, b; cin>>a>>b;
     int x = a >= b ? (a - b) : (b - a);
     int steps = x % 10 == 0 ? x/10 : (x/10 + 1);
     cout<<steps<<endl;
