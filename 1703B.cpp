d3603999b2232b2f7572fb39005da04a89785c32
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 23:17:30 +0530

    added

--- 1703B.cpp
+++ 1703B.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -11,17 +9,13 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
-    string s;
-    cin>>s;
+    int n; cin>>n;
+    string s; cin>>s;
     unordered_set<char> uos;
-    for(char c : s){
+    for(char c : s)
       uos.insert(c);
-    }
     cout<<n+uos.size()<<endl;
   }
 
