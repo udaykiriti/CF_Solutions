dd6ec34de80ab4e0af8bd4e02857167f8c4f47f9
udaykiriti <udaykiriti624@gmail.com>
2025-06-24 12:38:42 +0530

    restoring three numbers

--- Median_Maximization.cpp
+++ Median_Maximization.cpp
@@ -1,18 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
 
 int main(){
-  int t;
-  cin>>t;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   int x, s, n;
   for(int i = 0; i < t; i++){
     cin>>n>>s;
-    if(n%2 == 0){
+    if(n%2 == 0)
       x = s/((n/2) + 1);
-    }
-    else{
+    else
       x = s/((n+1)/2);
-    }
     cout<<x<<endl;
   }
 }
