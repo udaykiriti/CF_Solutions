ec2e6ad6c255543e0c95131c84e2eb4349d01685
udaykiriti <udaykiriti624@gmail.com>
2025-06-26 13:46:47 +0530

    added

--- 1.cpp
+++ 1.cpp
@@ -44,8 +44,7 @@ using sc = set<char>;
 #define oms_rank(ms, val) ms.order_of_key({val, 0})
 
 // Fast I/O
-#define stop_sync ios::sync_with_stdio(false)
-#define untie_ios cin.tie(nullptr)
+#define FastIO() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
 // Output Utilities
 #define FIXED(x) cout << fixed << setprecision(x)
@@ -192,10 +191,8 @@ void solve() {
 
 // Main Function
 int main() {
-    stop_sync;
-    untie_ios;
-    int t;
-    cin >> t;
+    FastIO();
+    int t; cin >> t;
     while (t--) solve();
     return 0;
 }
