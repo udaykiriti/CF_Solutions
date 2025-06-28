522347746df84804cb142927ca6e1dbcc19068fb
udaykiriti <udaykiriti624@gmail.com>
2025-06-28 09:26:38 +0530

    added

--- 1360A.cpp
+++ 1360A.cpp
@@ -3,11 +3,12 @@ using namespace std;
 #define endl '\n';
 int main()
 {
+
+
+
   ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int t;
-  cin >> t;
+  cin.tie(0); cout.tie(0);
+  int t; cin >> t;
   int a, b, area;
   while (t--)
   {
@@ -15,24 +16,16 @@ int main()
     if (b <= a)
     {
       if (2 * b < a)
-      {
         area = a * a;
-      }
       else
-      {
         area = 4 * b * b;
-      }
     }
     else
     {
       if (2 * a < b)
-      {
         area = b * b;
-      }
       else
-      {
         area = 4 * a * a;
-      }
     }
     cout << area << endl;
   }
