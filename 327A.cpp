734b70f218e21a1859be94a650b89e103d31a988
udaykiriti <udaykiriti624@gmail.com>
2025-06-29 21:23:11 +0530

    Update C++ files: 327A

--- 327A.cpp
+++ 327A.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
+
 
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int n;
-  cin >> n;
+  cin.tie(0); cout.tie(0);
+  int n; cin >> n;
 
   vector<int> a(n);
   int totalOnes = 0;
@@ -19,8 +20,7 @@ int main()
       totalOnes++;
   }
 
-  int maxGain = -1;
-  int current = 0;
+  int maxGain = -1; int current = 0;
 
   for (int i = 0; i < n; ++i)
   {
@@ -30,13 +30,9 @@ int main()
   }
 
   if (totalOnes == n)
-  {
     cout << n - 1 << endl;
-  }
   else
-  {
     cout << totalOnes + maxGain << endl;
-  }
 
   return 0;
 }
