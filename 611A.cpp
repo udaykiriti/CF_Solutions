41b2af6a5aac2fbf521688721a74ffd99d8e9ab4
udaykiriti <udaykiriti624@gmail.com>
2025-06-29 16:07:35 +0530

    Optimize logic in 611A C++ files

--- 611A.cpp
+++ 611A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,30 +8,24 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
   string b, s;
   int n;
   int days = 0;
   cin>>n>>b>>s;
   if(s[0] == 'm'){
-    if(n == 31){
+    if(n == 31)
       cout<<7<<endl;
-    }
-    else if(n == 30){
+    else if(n == 30)
       cout<<11<<endl;
-    }
-    else{
+    else
       cout<<12<<endl;
-    }
   }
   else if(s[0] == 'w'){
-    if(n == 6 || n == 5){
+    if(n == 6 || n == 5)
       cout<<53<<endl;
-    }
-    else{
+    else
       cout<<52<<endl;
-    }
   }
-
 }
