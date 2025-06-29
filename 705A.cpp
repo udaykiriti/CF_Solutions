aaf402b9a4b693861a2be642b0d77f58110851f8
udaykiriti <udaykiriti624@gmail.com>
2025-06-29 16:08:36 +0530

    Optimize logic in 705A C++ files

--- 705A.cpp
+++ 705A.cpp
@@ -6,10 +6,8 @@ using namespace std;
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int n;
-  cin >> n;
+  cin.tie(NULL); cout.tie(NULL);
+  int n; cin >> n;
   string result = "";
   FOR(i, 0, n)
   {
