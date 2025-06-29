8eacd1b9956734540968bfc7cb5c1e66f22f4d03
udaykiriti <udaykiriti624@gmail.com>
2025-06-29 16:03:43 +0530

    Fix edge cases in C++ implementations of 368B

--- 368B.cpp
+++ 368B.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n';
+
 void solve()
 {
-  int n, m;
-  cin >> n >> m;
-
+  int n, m; cin >> n >> m;
   vector<int> a(n);
   for (int i = 0; i < n; ++i)
-  {
     cin >> a[i];
-  }
 
   vector<int> distinct_count(n + 1, 0);
   unordered_set<int> seen;
