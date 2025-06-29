1f71a92307a6757934314e3c4e7c1fc2940b0f0a
udaykiriti <udaykiriti624@gmail.com>
2025-06-29 16:11:25 +0530

    Optimize logic in 935A C++ files

--- 935A.cpp
+++ 935A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,15 +8,12 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
   
-  int n;
-  cin>>n;
+  int n; cin>>n;
   int count = 0;
   for(int i = 1; i < n; i++){
     (n % i == 0) ? count++ : count+=0;
   }
   cout<<count<<endl;
-
 }
