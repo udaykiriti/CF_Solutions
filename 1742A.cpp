a928d3895549e48a8d3df3cc055a9c3c95aee058
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:32:58 +0530

    Optimize logic in 1560C C++ files

--- 1742A.cpp
+++ 1742A.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 void solve()
 {
-  int a, b, c;
-  cin >> a >> b >> c;
-
+  int a, b, c; cin >> a >> b >> c;
   if (a + b == c || a + c == b || b + c == a)
     cout << "YES" << endl;
   else
@@ -16,9 +14,8 @@ void solve()
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
+  cin.tie(nullptr); cout.tie(NULL);
+  int t; cin >> t;
   while (t--)
     solve();
   return 0;
