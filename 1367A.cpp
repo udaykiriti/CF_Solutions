bedef591106325a248b2bfd98269d02ebef7a66c
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:33:29 +0530

    Optimize logic in 1367C C++ files

--- 1367A.cpp
+++ 1367A.cpp
@@ -7,14 +7,10 @@ void solve()
   cin >> t;
   while (t--)
   {
-    string b;
-    cin >> b;
+    string b; cin >> b;
     string a;
     a += b[0];
-    for (int i = 1; i < b.size(); i += 2)
-    {
-      a += b[i];
-    }
+    for (int i = 1; i < b.size(); i += 2) a += b[i];
     cout << a << "\n";
   }
 }
