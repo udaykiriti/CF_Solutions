5297830eb25e517309ea9c4194ba052f9d6553f2
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:43:36 +0530

    Optimize logic in 1399A C++ files

--- 1399A.cpp
+++ 1399A.cpp
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   vector<int> a(n);
   for (int i = 0; i < n; ++i)
-  {
     cin >> a[i];
-  }
   sort(a.begin(), a.end());
   bool possible = true;
   for (int i = 1; i < n; ++i)
@@ -21,24 +19,17 @@ void solve()
     }
   }
   if (possible)
-  {
     cout << "YES\n";
-  }
   else
-  {
     cout << "NO\n";
-  }
 }
 
 int main()
 {
   ios::sync_with_stdio(0);
-  cin.tie(0);
-  int t;
-  cin >> t;
+  cin.tie(0); cout.tie(NULL);
+  int t; cin >> t;
   while (t--)
-  {
     solve();
-  }
   return 0;
 }
