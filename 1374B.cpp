1ae959fe1932e114735741058752d26c9beb4a8e
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:38:25 +0530

    Optimize logic in 1374B C++ files

--- 1374B.cpp
+++ 1374B.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   long long n;
   while(t--){
     cin>>n;
