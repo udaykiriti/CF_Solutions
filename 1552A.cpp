54e337e01cadd8414d308c8890fd024d5a03d315
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:25:41 +0530

    Optimize logic in 1552A C++ files

--- 1552A.cpp
+++ 1552A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,26 +8,17 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  int t;
-  cin>>t;
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   string s, k;
   while(t--){
-    int n;
-    cin>>n;
-    cin>>s;
+    int n; cin>>n>>s;
     k = s;
     sort(s.begin(), s.end());
     int count = 0;
-    for(int i = 0; i < s.length(); i++){
-      if(s[i] != k[i]){
+    for(int i = 0; i < s.length(); i++)
+      if(s[i] != k[i])
         count++;
-      }
-    }
-
     cout<<count<<endl;
-
   }
-
 }
