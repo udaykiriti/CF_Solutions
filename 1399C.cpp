1688b5524d0dee6b49570f155130ff804e626669
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:45:31 +0530

    Optimize logic in 1399C C++ files

--- 1399C.cpp
+++ 1399C.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   vector<int> w(n);
   for (int i = 0; i < n; ++i)
     cin >> w[i];
@@ -16,10 +15,7 @@ void solve()
   {
     vector<int> freq(n + 1, 0);
     for (int weight : w)
-    {
       freq[weight]++;
-    }
-
     int teams = 0;
     for (int i = 1; i <= n; ++i)
     {
@@ -41,21 +37,15 @@ void solve()
     }
     max_teams = max(max_teams, teams);
   }
-
   cout << max_teams << endl;
 }
 
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  int t;
-  cin >> t;
+  cin.tie(nullptr); cout.tie(NULL);
+  int t; cin >> t;
   while (t--)
-  {
     solve();
-  }
-
   return 0;
 }
