007169490768fed5954671d0d6b3dc5446046ea1
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:32:18 +0530

    Optimize logic in 1560C C++ files

--- 1739B.cpp
+++ 1739B.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl "\n"
 #define debug(n) cout<<(n)<<endl;
@@ -9,16 +8,13 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  int t;
-  cin>>t;
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
+    int n; cin>>n;
     int drr[n];
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
       cin>>drr[i];
-    }
     int arr[n];
     int check = 0;
     arr[0] = drr[0];
@@ -31,21 +27,16 @@ int main(){
         break;
       }
       else{
-        if(a > 0){
+        if(a > 0)
           arr[i] = a;
-        }
-        else{
+        else
           arr[i] = b;
-        }
-      }
     }
-    if(check){
+    if(check)
       continue;
-    }
     else{
-      for(int i = 0; i < n; i++){
+      for(int i = 0; i < n; i++)
         cout<<arr[i]<<" ";
-      }
       cout<<endl;
     }
   }
