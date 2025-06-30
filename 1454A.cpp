844f5b3391bd1d7396b163f62d71e9c558da1fc4
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:48:54 +0530

    Optimize logic in 1454A C++ files

--- 1454A.cpp
+++ 1454A.cpp
@@ -1,15 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 int main(){
-  int t;
-  cin>>t;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   int n;
   while(t--){
     cin>>n;
-    for(int i = 2; i <= n; i++){
+    for(int i = 2; i <= n; i++)
       cout<<i<<" ";
-    }
     cout<<1<<endl;
   }
   return 0;
