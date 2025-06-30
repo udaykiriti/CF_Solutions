c92bad711d431273786588bcc2d83d51bd8e51b1
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:07:06 +0530

    Optimize logic in 1490A C++ files

--- 1490A.cpp
+++ 1490A.cpp
@@ -4,15 +4,12 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   vector<int> a(n);
-
   for (int i = 0; i < n; ++i)
     cin >> a[i];
 
   int insertions = 0;
-
   for (int i = 0; i < n - 1; ++i)
   {
     int x = a[i];
@@ -33,15 +30,12 @@ void solve()
 void FastIO()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 }
 int main()
 {
   FastIO();
-  int t;
-  cin >> t;
-
+  int t; cin >> t;
   while (t--)
     solve();
   return 0;
