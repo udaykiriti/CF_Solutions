cefcb5b4d5a4d034f4c512ee7e8e92da6c3cae1c
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:16:33 +0530

    Optimize logic in 1527A C++ files

--- 1527A.cpp
+++ 1527A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define FastIO                  \
   ios_base::sync_with_stdio(0); \
   cin.tie(0);                   \
-  cout.tie(0);
+  cout.tie(0)
 
 #define ll long long
 #define pb push_back
@@ -15,16 +15,14 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   int msb = log2(n);
   cout << ((1 << msb) - 1) << endl;
 }
 
 int main()
 {
-  FastIO int t;
-  cin >> t;
+  FastIO; int t; cin >> t;
   while (t--)
     solve();
   EXIT
