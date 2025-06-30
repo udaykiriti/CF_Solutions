1f03f006590435cfd23282b22eabb3ae60b3156d
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:24:43 +0530

    Optimize logic in 1551B1 C++ files

--- 1551B1.cpp
+++ 1551B1.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,23 +8,20 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   string s;
   int cnt[26];
   while(t--){
     cin>>s;
     memset(cnt, 0, sizeof(cnt));
     for(auto c : s) cnt[c - 'a']++;
-    int cnt1 = 0;
-    int cnt2 = 0;
+    int cnt1 = 0; int cnt2 = 0;
     for(auto i : cnt){
       if(i == 1) cnt1++;
       else if(i > 1) cnt2++;
     }
     cout<<(cnt2 + cnt1/2)<<endl;
   }
-
 }
