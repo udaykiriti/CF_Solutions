76f40aa531e53dfee4bea81f1a64b864504db209
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:41:17 +0530

    Optimize logic in 1389A C++ files

--- 1389A.cpp
+++ 1389A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,13 +8,10 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-
-  int t;
-  cin>>t;
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   while(t--){
-    int l, r;
-    cin>>l>>r;
+    int l, r; cin>>l>>r;
     if(r - l < l){
       cout<<-1<<" "<<-1<<endl;
       continue;
@@ -23,6 +19,5 @@ int main(){
     else{
       cout<<l<<" "<<(2*l)<<endl;
     }
-
   }
 }
