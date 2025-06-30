7e1c9210b3edaded030a926084446d487bd9415a
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:17:09 +0530

    Optimize logic in 1535A C++ files

--- 1535A.cpp
+++ 1535A.cpp
@@ -9,14 +9,12 @@ const ll INF = 2e18 + 99;
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin >> t;
+  int t; cin >> t;
   while (t--)
   {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
+    int a, b, c, d; cin >> a >> b >> c >> d;
     if (max(a, b) > min(c, d) && max(c, d) > min(a, b))
     {
       cout << "YES" << endl;
