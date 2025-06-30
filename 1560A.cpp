7bf630957a0480ee2be2118807976c0e13bb6ed8
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:27:41 +0530

    Optimize logic in 1560A C++ files

--- 1560A.cpp
+++ 1560A.cpp
@@ -5,15 +5,12 @@ using namespace std;
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int t;
-  cin >> t;
+  cin.tie(0); cout.tie(0);
+  int t; cin >> t;
   int n;
   while (t--)
   {
-    cin >> n;
-    int ans;
+    cin >> n; int ans;
     int i = 0, j = 1;
     while (i != n)
     {
