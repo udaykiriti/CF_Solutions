a2c7a7b132c78eff74dc27869bdbb16b474bf021
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:22:20 +0530

    Optimize logic in 1542A C++ files

--- 1542A.cpp
+++ 1542A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,23 +8,19 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
+    int n; cin>>n;
     int od_sum = 0, ev_sum = 0;
     int x;
     for(int i = 0; i < 2*n; i++){
       cin>>x;
-      if(x & 1){
+      if(x & 1)
         od_sum++;
-      }
-      else{
+      else
         ev_sum++;
-      }
     }
     if(od_sum == ev_sum){
       cout<<"YES"<<endl;
