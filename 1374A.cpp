4e8f0ce471c33f12ecb15dcfd451e2dae87774b6
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:37:54 +0530

    Optimize logic in 1374A C++ files

--- 1374A.cpp
+++ 1374A.cpp
@@ -4,22 +4,17 @@ using namespace std;
 
 void solve()
 {
-  long long x, y, n;
-  cin >> x >> y >> n;
-
+  long long x, y, n; cin >> x >> y >> n;
   long long m = (n - y) / x;
   long long k = m * x + y;
-
   cout << k << endl;
 }
 
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(0);
-  int t;
-  cin >> t;
+  cin.tie(nullptr); cout.tie(0);
+  int t; cin >> t;
   while (t--)
     solve();
   return 0;
