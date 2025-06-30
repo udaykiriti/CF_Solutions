741d0535d11dc4efdb51cded79dd92843186bfcd
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:47:22 +0530

    Optimize logic in 1426A C++ files

--- 1426A.cpp
+++ 1426A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,25 +8,20 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n, x;
-    cin>>n>>x;
+    int n, x; cin>>n>>x;
     if( n > 2){
         n -= 2;
-        if(n % x > 0){
+        if(n % x > 0)
           cout<<(n / x + 2)<<endl;
-        }
-        else{
+        else
           cout<<(n / x + 1)<<endl;
-        }
     }
     else{
       cout<<1<<endl;
     }
-
   }
 }
