6a9a2f8307069a286557c9ba0c33bb28f9ec6c41
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:02:13 +0530

    Optimize logic in 1472B C++ files

--- 1472B.cpp
+++ 1472B.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define debug(x) cout<<(x)<<endl
 
 void solve()
 {
-  int n, x, count1 = 0, count2 = 0;
-  cin >> n;
+  int n, x, count1 = 0, count2 = 0; cin >> n;
   while (n--)
   {
     cin >> x;
-    if (x == 1)
-      count1++;
-    else
-      count2++;
+    if (x == 1) count1++;
+    else count2++;
   }
   if ((count2 % 2 == 0 && count1 % 2 == 0) || (count2 % 2 != 0 && count1 % 2 == 0 && count1 > 0))
     cout << "YES" << endl;
@@ -22,10 +21,8 @@ void solve()
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int t;
-  cin >> t;
+  cin.tie(0); cout.tie(0);
+  int t; cin >> t;
   while (t--)
     solve();
   return 0;
