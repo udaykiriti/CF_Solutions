b3bf19e084127b1ee21a3badac513a9ac3001ada
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:12:56 +0530

    Optimize logic in 1520A C++ files

--- 1520B.cpp
+++ 1520B.cpp
@@ -5,17 +5,15 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   const int MAX = 10;
-  int t;
-  cin>>t;
+  int t; cin>>t;
   int n;
   while(t--){
     cin>>n;
     int arr[MAX];
     int r = log10(n);
     int tmp = 0;
-    for (int i = 0; i < r+1; i++) {
+    for (int i = 0; i < r+1; i++) 
       tmp = tmp * 10 + 1;
-    }
     cout<<(r * 9 + n / tmp)<<endl;
   }
   return 0;
