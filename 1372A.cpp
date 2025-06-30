b49e6c4107392f503e316da4c718fa8210cbbced
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:35:50 +0530

    Optimize logic in 1372A C++ files

--- 1372A.cpp
+++ 1372A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -11,14 +10,11 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
-    for(int i = 0; i < n; i++){
+    int n; cin>>n;
+    for(int i = 0; i < n; i++)
       cout<<1<<" ";
-    }
     cout<<endl;
   }
 
