57c7a09b882535a4ddda43cf9b06868e7a8c02f5
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 18:44:40 +0530

    Optimize logic in 1399B C++ files

--- 1399B.cpp
+++ 1399B.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-	int t;
-	cin >> t;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int t; cin >> t;
 	while(t--)
 	{
-    int n;
-		cin >> n;
+    int n; cin >> n;
     int a[n], b[n];
 		int mina = 0x3f3f3f3f, minb = 0x3f3f3f3f;
 		long long ans = 0;
@@ -23,9 +23,7 @@ int main()
 			minb = min(minb, b[i]);
 		}
 		for(int i = 1; i <= n; i++)
-		{
 			ans += 1ll * max(a[i] - mina, b[i] - minb);
-		}
 		cout << ans << endl;
 	}
 	return 0;
