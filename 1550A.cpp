af3464362febd0126d0f878be6d6ffc60d88232b
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 23:23:37 +0530

    Optimize logic in 1550A C++ files

--- 1550A.cpp
+++ 1550A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,14 +8,11 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    double n;
-    cin>>n;
+    double n; cin>>n;
     cout<<ceil(sqrt(n))<<endl;
   }
-
 }
