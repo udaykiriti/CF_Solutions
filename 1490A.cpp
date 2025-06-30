144fd9a136b3c1b5ac1a1d2fcb0d884f061d19df
udaykiriti <udaykiriti624@gmail.com>
2025-06-30 20:07:24 +0530

    Optimize logic in 1490A C++ files

--- 1490A.cpp
+++ 1490A.cpp
@@ -8,23 +8,19 @@ void solve()
   vector<int> a(n);
   for (int i = 0; i < n; ++i)
     cin >> a[i];
-
   int insertions = 0;
   for (int i = 0; i < n - 1; ++i)
   {
     int x = a[i];
     int y = a[i + 1];
-
     int small = min(x, y);
     int large = max(x, y);
-
     while (large > 2 * small)
     {
       small *= 2;
       ++insertions;
     }
   }
-
   cout << insertions << endl;
 }
 void FastIO()
