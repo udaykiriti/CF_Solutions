e3008beb7817016bcb7db57224b0155256ea7c7a
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:56:10 +0530

    Optimize logic in 723A C++ files

--- 723A.cpp
+++ 723A.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(0);
-  int a, b, c;
-  cin >> a >> b >> c;
+  cin.tie(NULL); cout.tie(0);
+  int a, b, c; cin >> a >> b >> c;
   int x[] = {a, b, c};
   sort(x, x + 3);
   cout << (x[2] - x[0]) << endl;
