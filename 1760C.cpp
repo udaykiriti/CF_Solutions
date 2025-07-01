aa7b15fd1c8ccbef36d952e65ed648e83e29d846
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:48:33 +0530

    Optimize logic in 1760C C++ files

--- 1760C.cpp
+++ 1760C.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 
+
 int main()
 {
 
-  long t;
-  scanf("%ld", &t);
+  long t; scanf("%ld", &t);
   while (t--)
   {
-    long n;
-    scanf("%ld", &n);
+    long n; scanf("%ld", &n);
     std::vector<long> v(n);
     long mxa(0), mxb(0);
     for (long p = 0; p < n; p++)
@@ -24,11 +23,8 @@ int main()
         mxb = v[p];
       }
     }
-
     for (long p = 0; p < n; p++)
-    {
       printf("%ld ", v[p] - ((v[p] == mxa) ? mxb : mxa));
-    }
     puts("");
   }
 }
