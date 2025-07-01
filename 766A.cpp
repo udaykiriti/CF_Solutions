b711ac4c979d6eb866dbe5eed9e22e6c1805bcb0
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:59:27 +0530

    Optimize logic in 766A C++ files

--- 766A.cpp
+++ 766A.cpp
@@ -6,9 +6,7 @@ using namespace std;
 
 void solve()
 {
-  string s1, s2;
-  cin >> s1 >> s2;
-
+  string s1, s2; cin >> s1 >> s2;
   if (s1.size() != s2.size())
     cout << max(s1.size(), s2.size()) << endl;
   else if (s1 == s2)
