42a8e52c2bb8846b8bf4b0bf43a021c1affea3b0
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 16:53:56 +0530

    Optimize logic in 1996A  C++ files

--- 1996A.cpp
+++ 1996A.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   int cows = n / 4;
   if (n % 4 == 2)
     cout << cows + 1 << "\n";
@@ -16,10 +15,8 @@ void solve()
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  int t;
-  cin >> t;
+  cin.tie(nullptr); cout.tie(nullptr);
+  int t; cin >> t;
   while (t--)
     solve();
   return 0;
