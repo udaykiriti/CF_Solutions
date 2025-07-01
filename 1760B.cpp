01ee25a2548663648eff585273acd28c690a4ea4
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:47:50 +0530

    Optimize logic in 1760B C++ files

--- 1760B.cpp
+++ 1760B.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,20 +8,15 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
-    string s;
-    cin>>s;
+    int n; cin>>n;
+    string s; cin>>s;
     int ans = 0;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
       ans = max(ans, s[i] - 'a' + 1);
-    }
     cout<<ans<<endl;
   }
-
 }
