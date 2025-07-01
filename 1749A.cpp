2e0cda3d3905e6c182223b1857149a2432b8f066
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:45:23 +0530

    Optimize logic in 1749A C++ files

--- 1749A.cpp
+++ 1749A.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,26 +8,18 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
-    int m;
-    cin>>m;
+    int n; cin>>n;
+    int m; cin>>m;
     int rooks[m][2];
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < m; i++)
       cin>>rooks[i][0]>>rooks[i][1];
-    }
-    if(m < n){
+    if(m < n)
       cout<<"YES"<<endl;
-    }
-    else{
+    else
       cout<<"NO"<<endl;
-    }
-
   }
-
 }
