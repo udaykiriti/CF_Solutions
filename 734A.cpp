25488f30dd32d3cd5b647fb49cb1776fe0793ecf
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 06:57:30 +0530

    Optimize logic in 734A C++ files

--- 734A.cpp
+++ 734A.cpp
@@ -57,12 +57,9 @@ using namespace std;
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
   int n, a = 0, d = 0;
-  cin >> n;
-  string s;
-  cin >> s;
+  cin >> n; string s; cin >> s;
   for (int i = 0; i < n; i++)
   {
     IF(s[i] == 'A', a++);
