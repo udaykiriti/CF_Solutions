179418ac546aa8664303916b3d15ad0037713e55
udaykiriti <udaykiriti624@gmail.com>
2025-07-01 21:17:11 +0530

    partial test cases in 2123F

--- 2123F.cpp
+++ 2123F.cpp
@@ -1,16 +1,16 @@
+//Wrong solution partial test cases 
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr); cout.tie(NULL);
 
-    int T;                         // number of test cases
-    if (!(cin >> T)) return 0;
-
+    int T; if (!(cin >> T)) return 0;
     const int N = 100000;
     vector<int> spf(N + 1);
-    iota(spf.begin(), spf.end(), 0);          // smallest prime factor sieve
+    iota(spf.begin(), spf.end(), 0);      
     for (int i = 2; i * 1LL * i <= N; ++i)
         if (spf[i] == i)
             for (int j = i * i; j <= N; j += i)
@@ -26,8 +26,7 @@ int main() {
         vector<int> p(n + 1, 0), used(n + 1, 0);
         vector<int> primes;
         for (int i = 2; i <= n; ++i) if (spf[i] == i) primes.push_back(i);
-        reverse(primes.begin(), primes.end());           // descending order
-
+        reverse(primes.begin(), primes.end());         
         for (int pr : primes) {
             vector<int> bucket;
             for (int m = pr; m <= n; m += pr)
@@ -41,7 +40,6 @@ int main() {
             }
         }
         for (int i = 1; i <= n; ++i) if (!p[i]) p[i] = i;
-
         for (int i = 1; i <= n; ++i)
             cout << p[i] << (i == n ? '\n' : ' ');
     }
