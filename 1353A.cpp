940493c5e1052652d3ec90f80cacf83f2d6b268b
udaykiriti <udaykiriti624@gmail.com>
2025-07-02 07:59:48 +0530

    Updated logic for 1353A.cpp

--- 1353A.cpp
+++ 1353A.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define endl '\n'
 
 int main(){
-  int t;
-  cin>>t;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int t; cin>>t;
   int n, m;
   while (t--) {
     cin>>n>>m;
