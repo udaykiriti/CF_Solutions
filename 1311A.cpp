6feafe5326be5a80c169b0ae8ee7dae002e2b940
udaykiriti <udaykiriti624@gmail.com>
2025-07-03 19:52:20 +0530

    Updated logic for 1311A.cpp

--- 1311A.cpp
+++ 1311A.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int a, b;
-    cin>>a>>b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int a, b; cin>>a>>b;
     if(a > b){
       if((a%2 == 0 && b%2 == 0) || (a%2 != 0 && b%2 != 0)){
         cout<<1<<endl;
