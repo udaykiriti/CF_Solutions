56adf0cabe221f36ffc6c1b543fef7018ac19ea8
udaykiriti <udaykiriti624@gmail.com>
2025-07-03 19:51:28 +0530

    Updated logic for 1525B.cpp

--- 1325B.cpp
+++ 1325B.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -9,13 +8,11 @@ const ll INF = 2e18 + 99;
 
 int main(){
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  cin.tie(NULL); cout.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n;
-    cin>>n;
+    int n; cin>>n;
     set <int, greater<int> > s1;
     int x;
     for(int i = 0; i < n; i++){
@@ -24,5 +21,4 @@ int main(){
     }
     cout<<s1.size()<<endl;
   }
-
 }
