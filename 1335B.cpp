d98385f653bed48df1c15001d25c35b684d72664
udaykiriti <udaykiriti624@gmail.com>
2025-07-04 19:56:20 +0530

    Updated logic for 1335B in CPP

--- 1335B.cpp
+++ 1335B.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 #define ll long long
 #define endl '\n'
 #define debug(n) cout<<(n)<<endl;
@@ -10,12 +9,9 @@ const ll INF = 2e18 + 99;
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-
-  int t;
-  cin>>t;
+  int t; cin>>t;
   while(t--){
-    int n, a, b;
-    cin>>n>>a>>b;
+    int n, a, b; cin>>n>>a>>b;
     string s = "";
     char c = 'a';
     for(int i = 0; i < n; i++){
@@ -27,5 +23,4 @@ int main(){
     }
     cout<<s<<endl;
   }
-
 }
