4c76254e03b13125fc58d0503bb1de8fb5ae79ab
udaykiriti <udaykiriti624@gmail.com>
2025-07-04 19:55:41 +0530

    Updated logic for 1335A in CPP

--- 1335A.cpp
+++ 1335A.cpp
@@ -4,22 +4,15 @@ using namespace std;
 
 void solve()
 {
-  int n;
-  cin >> n;
+  int n; cin >> n;
   cout << (n - 1) / 2 << endl;
 }
 
 int main()
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-
-  int t;
-  cin >> t;
-  while (t--)
-  {
-    solve();
-  }
+  cin.tie(0); cout.tie(0);
+  int t; cin >> t;
+  while (t--) solve();
   return 0;
 }
