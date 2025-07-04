91c71027701a6fed7fe696a759e9d54fda75b0b1
udaykiriti <udaykiriti624@gmail.com>
2025-07-04 19:52:25 +0530

    Updated logic for 1328A in C

--- 1328A.cpp
+++ 1328A.cpp
@@ -3,24 +3,17 @@ using namespace std;
 
 void solve()
 {
-
-  int a, b;
-  cin >> a >> b;
+  int a, b; cin >> a >> b;
   int remainder = a % b;
-  if (remainder == 0)
-    cout << 0 << endl;
-  else
-    cout << b - remainder << endl;
+  if (remainder == 0) cout << 0 << endl;
+  else cout << b - remainder << endl;
 }
 
 int main()
 {
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  int t;
-  cin >> t;
-  while (t--)
-    solve();
+  cin.tie(0); cout.tie(0);
+  int t; cin >> t;
+  while (t--) solve();
   return 0;
 }
