790424fcb89f3880d9561f50fb1bb846164efb9e
udaykiriti <udaykiriti624@gmail.com>
2025-07-04 19:53:52 +0530

    Updated logic for 1328B in CPP

--- 1328B.cpp
+++ 1328B.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 void solve()
 {
-  int n;
-  long long k;
-  cin >> n >> k;
-
+  ll n,k; cin >> n >> k;
   int pos1 = 0, pos2 = 0;
   for (int i = 1; i <= n; ++i)
   {
-    long long total = i * (i - 1LL) / 2;
+    ll total = i * (i - 1LL) / 2;
     if (total >= k)
     {
       pos1 = i;
@@ -18,8 +16,8 @@ void solve()
     }
   }
 
-  long long total_before = (pos1 - 1LL) * (pos1 - 2) / 2;
-  long long offset = k - total_before - 1;
+  ll total_before = (pos1 - 1LL) * (pos1 - 2) / 2;
+  ll offset = k - total_before - 1;
 
   int index1 = n - pos1;
   int index2 = n - offset - 1;
@@ -27,21 +25,14 @@ void solve()
   string result(n, 'a');
   result[index1] = 'b';
   result[index2] = 'b';
-
   cout << result << '\n';
 }
 
 int main()
 {
   ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  int t;
-  cin >> t;
-  while (t--)
-  {
-    solve();
-  }
-
+  cin.tie(nullptr); cout.tie(NULL);
+  int t; cin >> t;
+  while (t--) solve();
   return 0;
 }
