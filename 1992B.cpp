0611e5eb9e7614a227d75f25ecc00637de03b1a0
udaykiriti <udaykiriti624@gmail.com>
2025-07-07 08:03:09 +0530

    added 2124 contest colutions only B

--- 1992B.cpp
+++ 1992B.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define stop_sync ios::sync_with_stdio(false)
 #define untie_ios cin.tie(nullptr)
-#define ln "\n"
+#define endl '\n'
 #define pb push_back
 #define sz(x) int((x).size())
 #define All(x) (x).begin(), (x).end()
-#define multicase \
-  int t;          \
-  cin >> t;       \
-  while (t--)
 
 using i64 = long long;
 void solve()
@@ -25,14 +20,13 @@ void solve()
     total += (2 * x - 1);
   }
   total -= (2 * mx - 1);
-  cout << total << ln;
+  cout << total << endl;
 }
 
 int main()
 {
-  stop_sync;
-  untie_ios;
-  multicase
-    solve();
+  stop_sync; untie_ios;
+  int t; cin>>t;
+  while(t--) solve();
   return 0;
 }
