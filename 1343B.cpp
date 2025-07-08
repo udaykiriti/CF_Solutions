ff00f1f722ae2c4cae1105d631f08f0af889811b
udaykiriti <udaykiriti624@gmail.com>
2025-07-08 14:52:35 +0530

    syntax updated

--- 1343B.cpp
+++ 1343B.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n';
+#define ll long long
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
-  int t;  cin >> t;
+  ll t;  cin >> t;
   while (t--)
   {
-    int n; cin >> n;
+    ll n; cin >> n;
     if ((n / 2) % 2 != 0)
     {
       cout << "NO" << endl;
@@ -18,9 +19,9 @@ int main()
     else
     {
       cout << "YES" << endl;
-      for (int i = 1; i <= n / 2; i++)
+      for (ll i = 1; i <= n / 2; i++)
         cout << (2 * i) << " ";
-      for (int i = 1; i < n / 2; i++)
+      for (ll i = 1; i < n / 2; i++)
         cout << (2 * i - 1) << " ";
       cout << (n / 2 + n - 1) << endl;
     }
--- 1343C.cpp
+++ 1343C.cpp
@@ -4,27 +4,26 @@ using namespace std;
 
 void solve()
 {
-  int n; cin >> n;
-  vector<int> a(n);
-  for (int &x : a)
+  ll n; cin >> n;
+  vector<ll> a(n);
+  for (ll &x : a)
     cin >> x;
   ll sum = 0;
-  int current = a[0];
-
-  for (int i = 1; i < n; ++i)
+  ll curr = a[0];
+  for (ll i = 1; i < n; ++i)
   {
-    if ((a[i] > 0 && current > 0) || (a[i] < 0 && current < 0))
+    if ((a[i] > 0 && curr > 0) || (a[i] < 0 && curr < 0))
     {
-      current = max(current, a[i]);
+      curr = max(curr, a[i]);
     }
     else
     {
-      sum += current;
-      current = a[i];
+      sum += curr;
+      curr = a[i];
     }
   }
 
-  sum += current;
+  sum += curr;
   cout << sum << "\n";
 }
 
