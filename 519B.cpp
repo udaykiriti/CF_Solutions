93322699c09fe2334d42b050a4d1a7714bfdd72f
udaykiriti <udaykiriti624@gmail.com>
2025-07-29 19:37:26 +0530

    519B.cpp

--- 519B.cpp
+++ 519B.cpp
@@ -1,37 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-
-void solve()
-{
-  int n; cin >> n;
-  vector<ll> a(n), b(n - 1), c(n - 2);
-  ll sum1 = 0, sum2 = 0, sum3 = 0;
-
-  for (ll i = 0; i < n; ++i)
-  {
-    cin >> a[i];
-    sum1 += a[i];
-  }
-  for (ll i = 0; i < n - 1; ++i)
-  {
-    cin >> b[i];
-    sum2 += b[i];
-  }
-  for (ll i = 0; i < n - 2; ++i)
-  {
-    cin >> c[i];
-    sum3 += c[i];
-  }
-  cout << sum1 - sum2 << endl;
-  cout << sum2 - sum3 << endl;
-}
+#define endl '\n'
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-  int t = 1; cin>>t;
-  while (t--) solve();
-  return 0;
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(0);
+    int n, t; cin >> n;
+    int a = 0, b = 0, c = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t;
+        a += t;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> t;
+        b += t;
+    }
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        cin >> t;
+        c += t;
+    }
+    cout << a - b << endl;
+    cout << b - c << endl;
+    return 0;
 }
